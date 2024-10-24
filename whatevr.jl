3a6106dae96cabb2ce0c8186eea3c13c8b34a93d
schulace <schulace@users.noreply.github.com>
2024-10-24 15:58:20 +0000

    Deploying to gh-pages from @ schulace/data_science_hw@f93042774e5d3e75f8fb037c4930d362b11284f3 🚀

---
+++ whatevr.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.17.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 849ca351-ca4a-4826-bfae-0fec8dcad760
+i'm new here!
+
+# ╔═╡ Cell order:
+# ╠═849ca351-ca4a-4826-bfae-0fec8dcad760
